45c6f58702ca20ef59ac5b197e23c24f25a38e34
Vikas Kumawat <121717195+itsoksonu@users.noreply.github.com>
2023-12-15 21:33:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,148 @@
+import  express   from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import multer from "multer";
+
+const app = express();
+const port = 3000;
+const mongoURL = "mongodb://127.0.0.1:27017/firstEd";
+
+// Connection with mongodb localhost
+mongoose.connect(mongoURL);
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', async(req, res)=>{
+    res.render("index.ejs");
+});
+
+// notes 
+app.get('/Computer_Science_Engineering_Notes', async(req,res)=>{
+    res.render('Computer Science & Engineering Notes.ejs')
+})
+app.get('/Electrical_Engineering_Notes', async(req,res)=>{
+    res.render('Electrical Engineering Notes.ejs')
+})
+app.get('/Civil_Engineering_Notes', async(req,res)=>{
+    res.render('Civil Engineering Notes.ejs')
+})
+app.get('/Mechanical_Engineering_Notes', async(req,res)=>{
+    res.render('Mechanical Engineering Notes.ejs')
+})
+app.get('/Electronic_Communication_Engineering_Notes', async(req,res)=>{
+    res.render('Electronics & Communication Engineering Notes.ejs')
+})
+app.get('/Information_Technology_Notes', async(req,res)=>{
+    res.render('Information Technology Notes.ejs')
+})
+app.get('/Computer_Science_Engineering_Cyber_Security_Notes', async(req,res)=>{
+    res.render('Computer Science & Engineering (Cyber Security) Notes.ejs')
+})
+app.get('/Electronics_Instrumentation_Engineering_Notes', async(req,res)=>{
+    res.render('Electronics & Instrumentation Engineering Notes.ejs')
+})
+app.get('/Impotrant_Study_Links', async(req,res)=>{
+    res.render('Impotrant Study Links.ejs')
+})
+
+// pervious year papers routes
+app.get('/Computer_Science_Engineering', async(req,res)=>{
+    res.render('Computer Science & Engineering.ejs')
+})
+app.get('/Electrical_Engineering', async(req,res)=>{
+    res.render('Electrical Engineering.ejs')
+})
+app.get('/Civil_Engineering', async(req,res)=>{
+    res.render('Civil Engineering.ejs')
+})
+app.get('/Mechanical_Engineering', async(req,res)=>{
+    res.render('Mechanical Engineering.ejs')
+})
+app.get('/Electronic_Communication_Engineering', async(req,res)=>{
+    res.render('Electronics & Communication Engineering.ejs')
+})
+app.get('/Information_Technology', async(req,res)=>{
+    res.render('Information Technology.ejs')
+})
+app.get('/Computer_Science_Engineering_Cyber_Security', async(req,res)=>{
+    res.render('Computer Science & Engineering (Cyber Security).ejs')
+})
+app.get('/Electronics_Instrumentation_Engineering', async(req,res)=>{
+    res.render('Electronics & Instrumentation Engineering.ejs')
+})
+
+//about and contact routes
+app.get('/about', async(req,res)=>{
+    res.render('about.ejs')
+})
+app.get('/contact', async(req,res)=>{
+    res.render('contact.ejs')
+})
+
+const {Schema} = mongoose;
+
+
+
+const fileSchema = new Schema({
+    name: {
+        type: String
+    },
+    data: {
+        type: Buffer
+    },
+    contentType: {
+        type: String
+    },
+});
+  
+const File = mongoose.model('File', fileSchema);
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage })
+
+app.post('/submit_file', upload.single('notesFile'),  async (req, res)=> {
+    // console.log(req.file);
+    try{
+        const { originalname, buffer, mimetype } = req.file;
+        const my_title = req.body.title
+        const my_description = req.body.description
+
+        const newFile = new File({
+            name: originalname,
+            data: buffer,
+            contentType: mimetype,
+        });
+
+        await newFile.save();
+
+        res.render('index.ejs', {message:"File uploaded successfully!"})
+    }
+    catch{
+        res.redirect("/")
+    }
+
+});
+
+app.get('/files/:id', async (req, res) => {
+  const fileId = req.params.id;
+
+  try {
+    const file = await File.findById(fileId);
+
+    if (!file) {
+      return res.status(404).send('File not found');
+    }
+
+    res.setHeader('Content-Type', file.contentType);
+    res.send(file.data);
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on port: http://localhost:${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const dropdowns = document.querySelectorAll(".dropdown");
+  
+  dropdowns.forEach(function(dropdown) {
+    dropdown.addEventListener("click", function() {
+      this.querySelector(".dropdown-content").classList.toggle("show");
+    });
+  });
+});
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.addEventListener("click", function(event) {
+  if (!event.target.matches(".dropdown")) {
+    const dropdowns = document.querySelectorAll(".dropdown-content");
+    dropdowns.forEach(function(dropdown) {
+      if (dropdown.classList.contains("show")) {
+        dropdown.classList.remove("show");
+      }
+    });
+  }
+});
+
+// Get the "return to top" button element
+var returnToTopButton = document.getElementById('returnToTop');
+
+// Function to scroll to the top of the page
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+
+// Function to toggle the visibility of the "return to top" button
+function toggleReturnToTopButton() {
+  if (window.scrollY > 200) {
+    returnToTopButton.classList.add('active');
+  } else {
+    returnToTopButton.classList.remove('active');
+  }
+}
+
+// Add event listeners
+window.addEventListener('scroll', toggleReturnToTopButton);
+returnToTopButton.addEventListener('click', scrollToTop);
+
+
+
+
