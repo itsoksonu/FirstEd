3daf425237d100c962a99dc237974277ce29e552
Vikas Kumawat <121717195+itsoksonu@users.noreply.github.com>
2023-12-15 22:09:48 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -46,6 +46,19 @@ function toggleReturnToTopButton() {
 window.addEventListener('scroll', toggleReturnToTopButton);
 returnToTopButton.addEventListener('click', scrollToTop);
 
+// build.js
+const execSync = require('child_process').execSync;
+
+// Your build commands
+try {
+  execSync('npm install');
+  execSync('npm run compile');
+  // Add more commands as needed
+} catch (error) {
+  console.error('Build failed:', error.message);
+  process.exit(1);
+}
+
 
 
 
