ee39fa53d6e35114b326716340e217fca5351267
Vikas Kumawat <121717195+itsoksonu@users.noreply.github.com>
2023-12-20 22:14:11 +0530

    Update build.js

--- build.js
+++ build.js
@@ -4,7 +4,6 @@ import { execSync } from 'child_process';
 // Your build commands
 try {
   execSync('npm install');
-  execSync('npm run compile');
   // Add more commands as needed
 } catch (error) {
   console.error('Build failed:', error.message);
